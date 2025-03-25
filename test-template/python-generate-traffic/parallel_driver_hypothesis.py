fba3095b459bcc69ebd7c21ac91f116649b6f6de
Connor McKee <connor.mckee@antithesis.com>
2025-03-25 11:13:03 -0400

    wrap hypothesis calls in try/except; update docker-compose perms

--- test-template/python-generate-traffic/parallel_driver_hypothesis.py
+++ test-template/python-generate-traffic/parallel_driver_hypothesis.py
@@ -36,10 +36,13 @@ def test_etcd(key, value):
         return
     requests = helper.generate_requests(1, 0, REQUEST_PROBABILITIES)
     for request in requests: 
-        success, error = helper.put_request(client, key, value) 
-        sometimes(success, "Client can make successful input-fuzzed put requests", None)
-        if not success: 
-            print(f"Client [parallel_driver_hypothesis]: unsuccessful put with key '{key}', value '{value}', and error '{error}'")
+        try: 
+            success, error = helper.put_request(client, key, value) 
+            sometimes(success, "Client can make successful input-fuzzed put requests", None)
+            if not success: 
+                print(f"Client [parallel_driver_hypothesis]: unsuccessful put with key '{key}', value '{value}', and error '{error}'")
+        except Exception as e:
+            print(f"Client [parallel_driver_hypothesis]: Exception {e}")
 
 if __name__ == '__main__':
     test_etcd()
