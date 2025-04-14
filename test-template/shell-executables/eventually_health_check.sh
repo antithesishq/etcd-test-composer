dfeb29e3d25fb1053ef6cdcbc1f49f6c2cf6a569
elixir-1 <katkoriavidhi@gmail.com>
2025-04-14 16:54:09 -0400

    Add source code for etcd tutorial part 1 - setup

--- test-template/shell-executables/eventually_health_check.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-java -cp /opt/antithesis/java-health-check/target/lib/*:/opt/antithesis/java-health-check/target/* com.antithesis.etcd.validation.EventuallyValidation
--- test-template/shell-executables/finally_db_consistent_lines.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cargo run --manifest-path=/opt/antithesis/rust-db-consistent-lines/Cargo.toml
